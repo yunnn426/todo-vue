6a9522e48ccf2c5e36d26d087c4954a3b967ece1
yunnn426 <yunnn99@gmail.com>
2024-10-04 00:13:08 +0900

    init: todo app initialize

---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<script setup>
+    defineProps({
+        tasks: Array,
+    })
+    
+</script>
+
+<template>
+    <div class="todo-list">
+        <h1 class="todo-title">---- Todo List ----</h1>
+        <div class="todo" v-for="task in tasks">
+            <input type="checkbox">
+                <label for="checkbox">&nbsp;{{ task }}</label>
+            </input>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.todo-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-image: url('../assets/paper.png');
+    background-size : 444px 900px;
+    background-position: center top;
+    background-repeat: no-repeat;
+
+    min-height: 1040px;
+    padding: 20px;
+    color: #606060;
+}
+
+.todo-title {
+    font-family: 'watermelon';
+    font-size: xxxx-large;
+}
+
+.todo {
+    display: flex;
+    padding: 10px;
+    width: 250px;
+}
+</style>
